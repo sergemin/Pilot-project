003248b13aa5963d5d7c46bad248e5cbacbdded4
sergemin <minin.serhii@gmail.com>
2016-10-06 10:44:40 +0300

    main page
    there is first commit

---
+++ js/myJQuery.js
@@ -0,0 +1,13 @@
+//(function($, undefined) {
+//    
+//    $('.topMenu').delegate('li', 'click', function() {
+//        alert('hello');
+//    })
+//    
+////    $('.menuShower').click(function() {
+////        $('.topMenu').toggleClass('transit');
+////    })
+//    $('.learnMoreButton').click(function() {
+//        $('.container1').fadeOut(2000);
+//    })
+//})(jQuery);
