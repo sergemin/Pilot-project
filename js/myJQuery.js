274c30886a32ca322e044e671535dc76129ca59e
Serhii Minin <minin.serhii@gmail.com>
2016-10-13 10:48:03 +0300

    Revert "Adaptivity"
    This reverts commit 376aae69291cd343d8a377ec16b0110d5bb5bf83.

--- js/myJQuery.js
+++ js/myJQuery.js
@@ -1,5 +1,13 @@
-;(function($, undefined) {
-    $('.showerTopMenu').click(function() {
-      $('.topMenu').toggleClass('openedMenu');
-    })
-})(jQuery);
+//(function($, undefined) {
+//    
+//    $('.topMenu').delegate('li', 'click', function() {
+//        alert('hello');
+//    })
+//    
+////    $('.menuShower').click(function() {
+////        $('.topMenu').toggleClass('transit');
+////    })
+//    $('.learnMoreButton').click(function() {
+//        $('.container1').fadeOut(2000);
+//    })
+//})(jQuery);
