9c497da885b392e4a8f346c224c4427f4124b664
Serhii Minin <minin.serhii@gmail.com>
2016-10-17 09:30:47 +0300

    Contact page
    Contact page's added. There are changes with js files. It will be going
    without jQuery

--- js/main.js
+++ js/main.js
@@ -1,27 +1,17 @@
-//var topMenu = document.querySelector('.topMenu');
-//topMenu.addEventListener('click', function(event) {
-//        var target = event.target;
-//        while(target.tagName != 'UL') {
-//            if(target.tagName != 'LI') return;
-//            alert('hell');
-//            target = target.parentNode;
-//        }
-//});
-(function() {
-    var primer = document.querySelector('li');
-    for(var i = 0, k = primer.lenght; i<k; i++) {
-        
-    }
-    primer.addEventListener('click', function(e) {
+;(function() {
+   var mainMenu = document.querySelector('.topMenu');
+    mainMenu.addEventListener('click', function(event) {
         var target = event.target;
-        while(target.tagName != 'UL') {
-        if(target.tagName != 'LI') {
-            alert(target.innerHTML);
-            return;
+        
+        while(target.tagName != 'Ul') {
+            if(target.tagName == 'LI') {
+                alert(target.innerHTML);
+                return;
+            }
+    
+            
+            target = target.parentNode;
         }
         
-            
-        target = target.parentNode();
-        }
-    })
-})()
+    });
+})();
