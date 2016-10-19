1c6e4d9de2a5d4072923b3dc0e6d247cc4a895e4
Serhii Minin <minin.serhii@gmail.com>
2016-10-19 09:30:02 +0300

    Working with Top Menu
    I've added adaptability to top menu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 ;(function() {
-   var mainMenu = document.querySelector('.topMenu');
-    mainMenu.addEventListener('click', function(event) {
-        var target = event.target;
-        
-        while(target.tagName != 'Ul') {
-            if(target.tagName == 'LI') {
-                alert(target.innerHTML);
-                return;
-            }
     
-            
-            target = target.parentNode;
-        }
-        
-    });
+    console.log('Width of screen = ' + document.documentElement.clientWidth + 'px');
+    
+    console.log('Width of screen with scroll = ' + document.documentElement.scrollWidth + 'px');
+    
+    var widthScreenCurent = document.documentElement.scrollWidth,
+        topMenu = document.querySelector('.topMenu');
+    if(widthScreenCurent > 847) {
+        topMenu.classList.remove('topMenuChanger');
+    }
+    var menuButton = document.querySelector('.topMenuShower');
+    
+    menuButton.addEventListener('click', function() {
+        topMenu.classList.toggle('topMenuChanger');
+    })
+    
+    
+//   var mainMenu = document.querySelector('.topMenu');
+//    mainMenu.addEventListener('click', function(event) {
+//        var target = event.target;
+//        
+//        while(target.tagName != 'Ul') {
+//            if(target.tagName == 'LI') {
+//                alert(target.innerHTML);
+//                return;
+//            }
+//    
+//            
+//            target = target.parentNode;
+//        }
+//        
+//    });
 })();
