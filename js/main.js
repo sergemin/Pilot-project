34389c82cde3a33838c3b0a2816b11d2fbb02aeb
Serhii Minin <minin.serhii@gmail.com>
2016-10-20 09:47:44 +0300

    Working with 860px screen
    I've changed header and footer

--- js/main.js
+++ js/main.js
@@ -1,34 +1,15 @@
 ;(function() {
+    var widthScreenCurent = document.documentElement.scrollWidth;
+    var topMenu = document.querySelector('.topMenu');
     
-    console.log('Width of screen = ' + document.documentElement.clientWidth + 'px');
-    
-    console.log('Width of screen with scroll = ' + document.documentElement.scrollWidth + 'px');
-    
-    var widthScreenCurent = document.documentElement.scrollWidth,
-        topMenu = document.querySelector('.topMenu');
-    if(widthScreenCurent > 847) {
-        topMenu.classList.remove('topMenuChanger');
-    }
+console.log(widthScreenCurent);
     var menuButton = document.querySelector('.topMenuShower');
     
+    if(widthScreenCurent < 843) {
+       topMenu.classList.remove('topMenuChanger');
+       }
     menuButton.addEventListener('click', function() {
         topMenu.classList.toggle('topMenuChanger');
     })
     
-    
-//   var mainMenu = document.querySelector('.topMenu');
-//    mainMenu.addEventListener('click', function(event) {
-//        var target = event.target;
-//        
-//        while(target.tagName != 'Ul') {
-//            if(target.tagName == 'LI') {
-//                alert(target.innerHTML);
-//                return;
-//            }
-//    
-//            
-//            target = target.parentNode;
-//        }
-//        
-//    });
 })();
