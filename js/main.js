58d7c19ebf4cc6ff967d20db4c460c6527bbcf3b
Serhii Minin <minin.serhii@gmail.com>
2016-10-21 10:41:14 +0300

    appearance of top menu
    working with appearance top menu for three of screen's sizes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 ;(function() {
     var widthScreenCurent = document.documentElement.scrollWidth;
     var topMenu = document.querySelector('.topMenu');
+    console.log(widthScreenCurent);
     
-console.log(widthScreenCurent);
+   
     var menuButton = document.querySelector('.topMenuShower');
+    
+    topMenu.addEventListener('click', function(event) {
+        var target = event.target;
+        //var class = event.getAttribute('className');
+        while(target != topMenu) { 
+           if (target.tagName == 'LI') {
+               // alert('hi');
+
+      return;
+    }
+    target = target.parentNode;
+        }
+    });
+    
     
     menuButton.addEventListener('click', function() {
         topMenu.classList.toggle('topMenuChanger');
@@ -11,4 +26,6 @@ console.log(widthScreenCurent);
     window.addEventListener('resize', function() {
         topMenu.classList.remove('topMenuChanger');
     })
+
+    
 })();
