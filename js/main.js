45318d655c5f8d4baa1c376e293a9ee504fb0293
Serhii Minin <minin.serhii@gmail.com>
2016-10-21 08:35:21 +0300

    Before changes for appearance of top menu
    I've decided to change appearance of top menu because I'd discovered
    that display property cannot work with transition

--- js/main.js
+++ js/main.js
@@ -5,11 +5,10 @@
 console.log(widthScreenCurent);
     var menuButton = document.querySelector('.topMenuShower');
     
-    if(widthScreenCurent < 843) {
-       topMenu.classList.remove('topMenuChanger');
-       }
     menuButton.addEventListener('click', function() {
         topMenu.classList.toggle('topMenuChanger');
     })
-    
+    window.addEventListener('resize', function() {
+        topMenu.classList.remove('topMenuChanger');
+    })
 })();
