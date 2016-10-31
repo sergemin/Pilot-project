e20f21c33551aeaacf01785ed76b48e0188c8aab
Serhii Minin <minin.serhii@gmail.com>
2016-10-31 11:14:57 +0200

    Working with Top Menu
    adding a code of top menu

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 ;(function() {
-    var widthScreenCurent = document.documentElement.scrollWidth;
-    var topMenu = document.querySelector('.topMenu');
-    console.log(widthScreenCurent);
     
+    
+    var widthScreenCurent = document.documentElement.scrollWidth;
+     console.log(widthScreenCurent);
+    
+    var topMenu = document.querySelector('.topMenu');
    
     var menuButton = document.querySelector('.topMenuShower');
     
@@ -23,23 +25,21 @@
     menuButton.addEventListener('click', function() {
         topMenu.classList.toggle('topMenuChanger');
     })
+    
+    //there is a code for hiding topMenu when user changes the size of screen
     window.addEventListener('resize', function() {
         topMenu.classList.remove('topMenuChanger');
     })
 
-    // Buttons for home page 4th
-    
-    var ourProj = document.querySelector('.buttonsOurProj');
-    
-    ourProj.addEventListener('click', function(event) {
+    // Buttons for home page 4th (gallery)
+    var buttonsGallery = document.querySelector('.buttonsOurProj');
+    buttonsGallery.addEventListener('click', function(event) {
         var target = event.target;
-       if(target.tagName != 'IMG') return;
-        var sizeTape = 10%;
-        var blocksForCont4Home = document.querySelector('.blocksForCont4Home');
         
-        blocksForCont4Home.setAttribute('style', 'margin-left:'+sizeTape);
-        sizeTape += sizeTape;
-        
+        while(target != buttonsGallery) { 
+           if (target.tagName == 'IMG') {
+        return;
+            }
+        }
     });
-    
 })();
